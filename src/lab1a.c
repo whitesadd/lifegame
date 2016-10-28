58145a704b9517df7299fdfc72c6075f2a22f042
whitesadd <david.blancop@gmail.com>
2016-10-28 22:43:10 +0200

    Implement Lab 1A, Quick and Dirty
    Change-Id: I27f3cd4aa34847909882e0d54509adb1f5972410

--- src/lab1a.c
+++ src/lab1a.c
@@ -33,42 +33,49 @@ int num_neighbors(int x, int y);
 int main(void)
 {
 	int n;
+	initialize_world();
 
-	/* TODO: initialize the world */
-
-
-	for (n = 0; n < NUM_GENERATIONS; n++)
+	for (n = 0; n < NUM_GENERATIONS; n++) {
+	    output_world();
 		next_generation();
+		sleep(1);
+	}
 
-	/* TODO: output final world state */
-
+	output_world();
 
 	return 0;
 }
 
 void next_generation(void) {
-	/* TODO: for every cell, set the state in the next
-	   generation according to the Game of Life rules
-
-	   Hint: use get_next_state(x,y) */
-
-
+    int x, y;
+    for (x = 0; x < get_world_width(); x++) {
+        for (y = 0; y < get_world_height(); y++) {
+            set_cell_state(x, y, get_next_state(x, y));
+        }
+    }
 	finalize_evolution(); /* called at end to finalize */
 }
 
 int get_next_state(int x, int y) {
-	/* TODO: for the specified cell, compute the state in
-	   the next generation using the rules
-
-	   Use num_neighbors(x,y) to compute the number of live
-	   neighbors */
-
+    if (get_cell_state(x, y) == ALIVE) {
+        if (num_neighbors(x, y) < 2) return DEAD;
+        if (num_neighbors(x, y) < 4) return ALIVE;
+        return DEAD;
+    } else {
+        if (num_neighbors(x, y) == 3) return ALIVE;
+        return DEAD;
+    }
 }
 
 int num_neighbors(int x, int y) {
-	/* TODO: for the specified cell, return the number of
-	   neighbors that are ALIVE
-
-	   Use get_cell_state(x,y) */
-
+    int neighborsCount = 0;
+    if (get_cell_state(x - 1, y - 1) == ALIVE) neighborsCount++;
+    if (get_cell_state(x - 1, y) == ALIVE) neighborsCount++;
+    if (get_cell_state(x - 1, y + 1) == ALIVE) neighborsCount++;
+    if (get_cell_state(x, y - 1) == ALIVE) neighborsCount++;
+    if (get_cell_state(x, y + 1) == ALIVE) neighborsCount++;
+    if (get_cell_state(x + 1, y - 1) == ALIVE) neighborsCount++;
+    if (get_cell_state(x + 1, y) == ALIVE) neighborsCount++;
+    if (get_cell_state(x + 1, y + 1) == ALIVE) neighborsCount++;
+    return neighborsCount;
 }
